0ca2a4d262bbaa40e3bf5a93e4d21468418ce865
Pringzheng <63899589+Pringzheng@users.noreply.github.com>
2023-01-31 01:05:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,3 @@
 sed -i 's/192.168.1.1/192.168.100.112/g' package/base-files/files/bin/config_generate
 
 # Install Argon Theme
-rm -rf package/lean/luci-theme-argon
-find ./ -name luci-theme-argon | xargs rm -rf;
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
